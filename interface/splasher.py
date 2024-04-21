28060fdda831ba409fc69000f44c8dca90eee715
Katherine Kerman <tonnam_s@outlook.com>
2024-04-21 12:36:07 +0700

    idk what to name these

--- interface/splasher.py
+++ interface/splasher.py
@@ -1,6 +1,3 @@
-import os
-import resource
-
 def start(): #returns the string of the start screen
     with open('interface/resource/splashScreren.txt',"rb") as file:
         fileContent = file.read()
