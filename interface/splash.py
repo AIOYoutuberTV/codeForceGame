06e6f8eaa264d4f59b3f7f5519299a717075dbd1
Katherine Kerman <tonnam_s@outlook.com>
2024-04-21 12:13:02 +0700

    I have no comments

--- interface/splash.py
+++ interface/splash.py
@@ -2,4 +2,12 @@ import os
 import resource
 
 def splash():
-    
+    with open('interface/resource/splashScreren.txt',"rt") as file:
+        fileContent = file.readlines()
+    i=0
+    while i<len(fileContent):
+        print(fileContent[i])
+        i+=1
+    file.close()
+
+splash()
